8753ca4450bf9a081f2fa40540e127841be1a7be
Mark Reid <mreid@mozilla.com>
2015-11-11 13:40:56 -0400

    more WIP

--- report/run_exec/input/stdin_heka.lua
+++ report/run_exec/input/stdin_heka.lua
@@ -5,25 +5,35 @@
 require "io"
 require "heka_stream_reader"
 require "string"
+require "snappy"
 
 local hsr = heka_stream_reader.new("stdin")
+local dhsr = heka_stream_reader.new("snappy")
 local cnt = 0
 
-function process_message()
-    local found, read, need = false, 0, 8192
-    while true do
-        local buf = io.stdin:read(need)
-        if not buf then break end
-
-        repeat
-            found, read, need = hsr:find_message(buf)
-            if found then
-                cnt = cnt + 1
-                inject_message(hsr)
-            end
-            buf = nil
-        until not found
+local function snappy_decode(msgbytes)
+    local ok, uc = pcall(snappy.uncompress, msgbytes)
+    if ok then
+        return uc
     end
+    return msgbytes
+end
 
-    return 0, tostring(cnt)
+function process_message()
+    local fh = io.stdin
+    local found, read, consumed
+    repeat
+        repeat
+            found, consumed, read = hsr:find_message(fh, false) -- don't protobuf decode
+            if found then
+                local pbm = snappy_decode(hsr:read_message("raw"))
+                local ok = pcall(dhsr.decode_message, dhsr, pbm)
+                if ok then
+                    inject_message(dhsr)
+                    cnt = cnt + 1
+                end
+            end
+        until not found
+    until read == 0
+    return 0
 end
